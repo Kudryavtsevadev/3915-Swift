d0e15d7a5891cb4f2bdc3cb4994be6194b536e40
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-11 14:16:31 +0200

    init

---
+++ ArcaneKeeper/App/ArcaneKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcaneKeeperApp.swift
+//  ArcaneKeeper
+//
+//  Created by Serhii Babchuk on 11.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArcaneKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AKLaunchView()
+        }
+    }
+}
